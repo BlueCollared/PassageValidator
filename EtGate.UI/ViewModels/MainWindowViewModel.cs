396e7da1aff1f55ff451c841705a2561d0d2f60f
AWadhva <anujwadhva@gmail.com>
2024-07-30 09:58:55 +0530

    Fixing UI

--- EtGate.UI/ViewModels/MainWindowViewModel.cs
+++ EtGate.UI/ViewModels/MainWindowViewModel.cs
@@ -2,8 +2,9 @@
 {
     public class MainWindowViewModel : ViewModelBase
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        public ViewModelBase CurrentModeViewModel { get; set; } = new AppBootingViewModel();
+//#pragma warning disable CA1822 // Mark members as static
+//        public string Greeting => "Welcome to Avalonia!";
+//#pragma warning restore CA1822 // Mark members as static
     }
 }
---
+++ EtGate.UI/Views/AppBootingView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace EtGate.UI.Views;
+
+public partial class AppBootingView : UserControl
+{
+    public AppBootingView()
+    {
+        InitializeComponent();
+    }
+}
